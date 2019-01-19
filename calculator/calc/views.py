311886a989954bb0207698321d37907fa50e1431
Akshitha <akshithagajawada12@gmail.com>
2019-01-19 14:37:25 +0530

    Calc project

---
+++ calculator/calc/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def calc(requests):
+    field1=requests.GET['field1']
+    field2=requests.GET['field2']
+    operation=requests.GET['optradio']
+    result=0
+    f1=int(field1)
+    f2=int(field2)
+    if operation=='+':
+        result=f1 + f2
+    elif operation=='-':
+        result=f1 - f2
+    elif operation=='*':
+        result=f1 * f2
+    elif operation=='/':
+        result=f1 / f2
+    return render(requests,'calc/calc.html',{'result':result})
+def home(requests):
+    return render(requests,'calc/home.html')
